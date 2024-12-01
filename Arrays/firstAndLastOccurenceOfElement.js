6d4495dea11bccd5177b660869cd2a87ebb4842c
nitin joshi <nj88329@gmail.com>
2024-12-01 16:44:18 +0530

    firstAndlastOccurenceofElement

---
+++ Arrays/firstAndLastOccurenceOfElement.js
@@ -0,0 +1,23 @@
+find(arr, x) {
+    // code here
+    
+    let firstOccurence = -1 ; let lastoccurence = -1;
+    
+    let ans = []
+    for( let i = 0 ; i < arr.length ; i ++)
+    {
+        if( arr[i] === x ) 
+           {
+               if(firstOccurence=== -1)
+               {
+                   firstOccurence = i;
+                   lastoccurence = i ;
+               }
+               else
+                 lastoccurence = i ;
+           }
+    }
+    
+    ans.push(firstOccurence , lastoccurence)
+    return ans;
+}
