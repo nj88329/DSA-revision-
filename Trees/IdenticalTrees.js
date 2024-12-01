45c0ab39c8d2925bb5c45da5ea61ce02d9b70d2a
nitin joshi <nj88329@gmail.com>
2024-12-01 16:57:57 +0530

    comments added

---
+++ Trees/IdenticalTrees.js
@@ -0,0 +1,23 @@
+isIdentical(r1, r2)
+ {
+    // function to check Identical Trees
+       function checkIdentical( r1 , r2 )
+       {
+           
+           if( r1 === null && r2 === null) return true;
+             
+            if( r1 === null || r2 === null ) return false;
+            
+            if( r1.data !== r2.data )
+            {
+                return false;
+            }
+           
+           
+        return checkIdentical( r1.left , r2.left ) && 
+            checkIdentical( r1.right , r2.right );
+            
+            
+       }
+ return    checkIdentical( r1 , r2 );
+}
