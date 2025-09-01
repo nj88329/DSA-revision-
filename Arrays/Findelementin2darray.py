efe1d54cf1314ed9a55074e6b96863f70eb63345
nitin joshi <nj88329@gmail.com>
2025-09-01 21:27:17 +0530

    python code for finding element in 2d array

---
+++ Arrays/Findelementin2darray.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])
+
+        start = 0 
+        end = row*col-1
+
+        while start <= end:
+            mid = (start + end)//2
+            m = mid//col
+            n = mid%col
+        
+            if matrix[m][n] == target:
+                return True
+
+            elif matrix[m][n] > target:
+                 end = mid-1
+
+            else:
+                start = mid + 1
+
+        return False     
